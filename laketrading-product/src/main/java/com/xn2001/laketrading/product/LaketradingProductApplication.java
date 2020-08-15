b8be3599733419c918a819dfb7f4bfe6a7e5912f
ZXH <jialna@qq.com>
2020-08-15 23:31:31 +0800

    公共模块

--- laketrading-product/src/main/java/com/xn2001/laketrading/product/LaketradingProductApplication.java
+++ laketrading-product/src/main/java/com/xn2001/laketrading/product/LaketradingProductApplication.java
@@ -1,9 +1,11 @@
 package com.xn2001.laketrading.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.xn2001.laketrading.product.dao")
 public class LaketradingProductApplication {
 
     public static void main(String[] args) {
--- laketrading-product/src/test/java/com/xn2001/laketrading/product/LaketradingProductApplicationTests.java
+++ laketrading-product/src/test/java/com/xn2001/laketrading/product/LaketradingProductApplicationTests.java
@@ -1,13 +1,24 @@
 package com.xn2001.laketrading.product;
 
+import com.xn2001.laketrading.product.entity.BrandEntity;
+import com.xn2001.laketrading.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LaketradingProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
+
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setDescript("哈哈1哈");
+        brandEntity.setName("华为");
+        brandService.save(brandEntity);
+        System.out.println("保存成功");
     }
 
 }
