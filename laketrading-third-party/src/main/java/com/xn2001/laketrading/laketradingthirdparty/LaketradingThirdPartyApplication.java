9a0584950712b925ce3ba57dd5b31dd35f2eaa4b
ZXH <jialna@qq.com>
2020-09-15 19:27:44 +0800

    上传服务

---
+++ laketrading-third-party/src/main/java/com/xn2001/laketrading/laketradingthirdparty/LaketradingThirdPartyApplication.java
@@ -0,0 +1,15 @@
+package com.xn2001.laketrading.laketradingthirdparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class LaketradingThirdPartyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaketradingThirdPartyApplication.class, args);
+    }
+
+}
---
+++ laketrading-third-party/src/main/java/com/xn2001/laketrading/laketradingthirdparty/controller/OssApplication.java
@@ -0,0 +1,71 @@
+package com.xn2001.laketrading.laketradingthirdparty.controller;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.common.utils.BinaryUtil;
+import com.aliyun.oss.model.MatchMode;
+import com.aliyun.oss.model.PolicyConditions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author 乐心湖
+ * @date 2020/9/15 17:47
+ **/
+@RestController
+public class OssApplication {
+
+    @Autowired
+    private OSS ossClient;
+
+    @Value ("${spring.cloud.alicloud.oss.endpoint}")
+    private String endpoint ;
+    @Value("${spring.cloud.alicloud.oss.bucket}")
+    private String bucket ;
+    @Value("${spring.cloud.alicloud.access-key}")
+    private String accessId;
+    @Value("spring.cloud.alicloud.secret-key")
+    private String secretKey;
+
+    @RequestMapping("/oss/policy")
+    public Map<String, String> policy(){
+        String host = "https://" + bucket + "." + endpoint; // host的格式为 bucketname.endpoint
+        String dir = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        Map<String,String> respMap = null;
+        try {
+            long expireTime = 30;
+            long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
+            Date expiration = new Date(expireEndTime);
+            PolicyConditions policyConds = new PolicyConditions();
+            policyConds.addConditionItem(PolicyConditions.COND_CONTENT_LENGTH_RANGE, 0, 1048576000);
+            policyConds.addConditionItem(MatchMode.StartWith, PolicyConditions.COND_KEY, dir);
+
+            String postPolicy = ossClient.generatePostPolicy(expiration, policyConds);
+            byte[] binaryData = postPolicy.getBytes("utf-8");
+            String encodedPolicy = BinaryUtil.toBase64String(binaryData);
+            String postSignature = ossClient.calculatePostSignature(postPolicy);
+
+            respMap= new LinkedHashMap<String, String>();
+            respMap.put("accessid", accessId);
+            respMap.put("policy", encodedPolicy);
+            respMap.put("signature", postSignature);
+            respMap.put("dir", dir);
+            respMap.put("host", host);
+            respMap.put("expire", String.valueOf(expireEndTime / 1000));
+
+        } catch (Exception e) {
+            // Assert.fail(e.getMessage());
+            System.out.println(e.getMessage());
+        } finally {
+            ossClient.shutdown();
+        }
+        return respMap;
+    }
+}
